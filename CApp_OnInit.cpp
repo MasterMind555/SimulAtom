e5974feb2518fde1e83f203b0fe1a976acbf96ed
MasterMind555 <MasterMind555@rocketmail.com>
2012-06-16 21:12:53 -0400

    Added a Header with constants

--- CApp_OnInit.cpp
+++ CApp_OnInit.cpp
@@ -1,12 +1,13 @@
 #include "CApp.h"
 #include "CSurface.h"
+#include "Constants.h"
 
 bool CApp::OnInit() {
     if(SDL_Init(SDL_INIT_EVERYTHING) < 0) {
         return false;
     }
 
-    if((screen = SDL_SetVideoMode(600, 600, 32, SDL_HWSURFACE | SDL_DOUBLEBUF)) == NULL) {
+    if((screen = SDL_SetVideoMode(screenWidth, screenHeight, 32, SDL_HWSURFACE | SDL_DOUBLEBUF)) == NULL) {
         return false;
     }
 
